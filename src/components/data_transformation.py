67f6475367edc921093d51a44ca9d7a6f2e9cafc
aditya <meenakshi.bhtt@gmail.com>
2023-03-24 07:15:37 +0530

    Data Transformation file updated with correct comments

---
+++ src/components/data_transformation.py
@@ -0,0 +1,3 @@
+# For the time being we don't have transformations we have to apply SMOTE But that pycaret will take care of.
+# That's the advantage of using Pycaret
+# Also suppose there were missing values and other related kinds of stuff everything can be handelled by Pycaret only.
