1e7c556b43c25c6dc64684eeed7fab0b3f342d1f
aditya <meenakshi.bhtt@gmail.com>
2023-03-27 07:44:36 +0530

    Data Tranformation module implemented

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -3,7 +3,10 @@ from dataclasses import dataclass
 
 import numpy as np 
 import pandas as pd
-
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer,FunctionTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder,StandardScaler
 
 from src.exception import CustomException
 from src.logger import logging
@@ -21,33 +24,26 @@ class DataTransformation:
 
     def get_data_transformer_object(self):
         '''
-        This function si responsible for data trnasformation
+        This function is resposible for data transformation.
         
         '''
         try:
-            numerical_columns = ["writing_score", "reading_score"]
-            categorical_columns = [
-                "gender",
-                "race_ethnicity",
-                "parental_level_of_education",
-                "lunch",
-                "test_preparation_course",
-            ]
-
+            numerical_columns = ["aluminium","ammonia","arsenic","barium","cadmium",
+                                "chloramine","chromium","copper","flouride","bacteria",
+                                "viruses","lead,nitrates","nitrites","mercury","perchlorate","radium","selenium",
+                                "silver","uranium"]
+            categorical_columns = ["is_safe"]
             num_pipeline= Pipeline(
                 steps=[
-                ("imputer",SimpleImputer(strategy="median")),
-                ("scaler",StandardScaler())
-
+            ("scaler",StandardScaler())
                 ]
             )
 
             cat_pipeline=Pipeline(
 
                 steps=[
-                ("imputer",SimpleImputer(strategy="most_frequent")),
-                ("one_hot_encoder",OneHotEncoder()),
-                ("scaler",StandardScaler(with_mean=False))
+                ("replace_bad_value",FunctionTransformer(lambda x: np.where(x == "#NUM!", np.nan, x))),
+                ("imputer",SimpleImputer(missing_values=np.nan,strategy="most_frequent"))
                 ]
 
             )
@@ -70,6 +66,8 @@ class DataTransformation:
         except Exception as e:
             raise CustomException(e,sys)
         
+
+        
     def initiate_data_transformation(self,train_path,test_path):
 
         try:
@@ -82,8 +80,11 @@ class DataTransformation:
 
             preprocessing_obj=self.get_data_transformer_object()
 
-            target_column_name="math_score"
-            numerical_columns = ["writing_score", "reading_score"]
+            target_column_name="is_safe"
+            numerical_columns = ["aluminium","ammonia","arsenic","barium","cadmium",
+                                "chloramine","chromium","copper","flouride","bacteria",
+                                "viruses","lead,nitrates","nitrites","mercury","perchlorate","radium","selenium",
+                                "silver","uranium"]
 
             input_feature_train_df=train_df.drop(columns=[target_column_name],axis=1)
             target_feature_train_df=train_df[target_column_name]
